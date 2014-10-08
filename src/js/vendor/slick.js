9c7f4a9c105624a154c53ef760783b1b75c579c3
360art <barw@o2.pl>
2014-10-08 19:15:07 +0200

    Filtr urojen gotowy slider desktop, bez trimowania tekstu bez WP

--- src/js/vendor/slick.js
+++ src/js/vendor/slick.js
@@ -1940,37 +1940,3 @@
     };
 
 }));
-;;(function() {
-
-	//Filtr urojen
-	var filtr_slider = $('.filtr__slider').slick({
-		vertical: true,
-		centerMode: true,
-	  	//centerPadding: '250px',
-	  	slidesToShow: 3,
-	  	arrows: false
-  	});
-
-	//Dim even sections
-	function dimText(filtr_set) {
-		filtr_set.filter( ":even" ).css('color','gray');
-		filtr_set.eq(1)	.css('color','#fff').fadeIn();
-	}
-
-	//Hide more section
-	//$('.filtr__more').hide();
-	//Problemy z ponownym wyliczeniem slidera
-
-	//Dim
-	dimText($('.slick-active'));
-
-	$('.filtr__arrow-up').click(function(){
-		filtr_slider.slickPrev();
-		dimText($('.slick-active'));
-	});
-
-	$('.filtr__arrow-down').click(function(){
-		filtr_slider.slickNext();
-		dimText($('.slick-active'));
-	});
-})();
--- src/js/website.scripts.js
+++ src/js/website.scripts.js
@@ -1,77 +1,34 @@
 ;(function() {
 
-	Detectizr.detect();
+	//Filtr urojen
+	var filtr_slider = $('.filtr__slider').slick({
+		vertical: true,
+		centerMode: true,
+	  	//centerPadding: '250px',
+	  	slidesToShow: 3,
+	  	arrows: false
+  	});
 
-	$('#nav').hidescroll({
-		offset: 0
-	});
-
-	$('.track__item').stick_in_parent();
-
-	$('#progressMessage').readingTime();
-
-	$('#commentsWrapper').on('hide.bs.collapse', function () {
-		$('#commentsToggle').find('span').html('Rozwiń komentarze');
-	}).on('show.bs.collapse', function () {
-		$('#commentsToggle').find('span').html('Zwiń komentarze');
-	});
-
-	$('#menuToggle, #overlay').on('click touchstart', function(event){
-		event.preventDefault();
-		$('#wrapper, #nav, #overlay, #footer, html, body').toggleClass('is-close is-open');
-
-		if ($('#menu').hasClass('is-open')) {
-			setTimeout(function () {
-				$('#menu').toggleClass('is-close is-open');
-			}, 750);
-		} else {
-			$('#menu').toggleClass('is-close is-open');
-		}
-
-	});
-
-	var slickOptions = {
-		dots: false,
-		speed: 600,
-		swipe: true,
-		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
-		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
-		cssEase: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)'
-	};
-
-	$('.slick--oneitem').slick($.extend({}, slickOptions, {
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		infinite: true
-	}));
-	$('.slick--fouritem').slick($.extend({}, slickOptions, {
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		infinite: true,
-		// responsive: [
-		// 	{
-		// 		breakpoint: 768,
-		// 		settings: {
-		// 			arrows: false
-		// 		}
-		// 	},
-		// 	{
-		// 		breakpoint: 480,
-		// 		settings: {
-		// 			slidesToShow: 4,
-		// 			slidesToScroll: 4,
-		// 			arrows: false
-		// 		}
-		// 	}
-		// ]
-	}));
-
-	if ($('html').hasClass('no-touch')) {
-		var s = skrollr.init({
-			forceHeight: false
-		});
+	//Dim even sections
+	function dimText(filtr_set) {
+		filtr_set.filter( ":even" ).css('color','gray');
+		filtr_set.eq(1)	.css('color','#fff').fadeIn();
 	}
 
+	//Hide more section
+	//$('.filtr__more').hide();
+	//Problemy z ponownym wyliczeniem slidera
+
+	//Dim
+	dimText($('.slick-active'));
+
+	$('.filtr__arrow-up').click(function(){
+		filtr_slider.slickPrev();
+		dimText($('.slick-active'));
+	});
+
+	$('.filtr__arrow-down').click(function(){
+		filtr_slider.slickNext();
+		dimText($('.slick-active'));
+	});
 })();
-
-
