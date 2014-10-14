ae83e031527c36f88d9a7b98a5908b708a588d40
360art <barw@o2.pl>
2014-10-14 12:09:27 +0200

    Proba wyswietlenia pola excerpt

--- src/index.php
+++ src/index.php
@@ -11,6 +11,10 @@
 // Posts
 $posts = Timber::get_posts();
 
+$args = array('post_type'=>'filtr', 'type' => 'excerpt');
+$filtry = Timber::get_posts($args);
+
 $theme->view('index', array(
 	'posts' => $posts,
+	'filtry' => $filtry,
 ));
