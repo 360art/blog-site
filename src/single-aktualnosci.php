cb692ddbf22acce64777e8e9da11a0cca68a5d7e
Bartosz Walicki <barw@o2.pl>
2014-11-16 12:31:58 +0100

    Dodano layout aktualnosci popup wedlug uwag klienta ( ͡°╭͜ʖ╮͡° )

--- src/single-aktualnosci.php
+++ src/single-aktualnosci.php
@@ -11,4 +11,12 @@
 $theme->view('single-aktualnosci', array(
 	'post' => new TimberPost(),
 	'settings' => get_option('my_option_name'),
+
+	'aktualnosci' => Timber::get_posts(array(
+		'post_type'=>'aktualnosci',
+		'limit' => '3',
+		'orderby' => 'date',
+		'order' => 'DESC'
+	)),
 ));
+
